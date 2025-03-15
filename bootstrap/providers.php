fe19f8939e8e12a2d61cc49e087e937886a7f974
Wakhid Wicaksono <wichaksono.w@gmail.com>
2025-03-15 07:09:37 +0700

    Adding Spatie Sortable

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    Spatie\EloquentSortable\EloquentSortableServiceProvider::class,
 ];
